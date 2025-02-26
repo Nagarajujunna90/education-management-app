d8594b762082e073b6e28deb3453e1e3e145d9bc
nagaraju.junna <nagaraju.junna90@gmail.com>
2025-02-26 16:13:53 +0530

    inital commit

---
+++ exam-report-service/src/main/java/com/nr/exam_report_service/ExamReportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nr.exam_report_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamReportServiceApplication.class, args);
+	}
+
+}
---
+++ exam-report-service/src/test/java/com/nr/exam_report_service/ExamReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nr.exam_report_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamReportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
