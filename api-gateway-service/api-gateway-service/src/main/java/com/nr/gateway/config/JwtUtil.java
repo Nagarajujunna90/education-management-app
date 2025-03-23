f5a53772f1ccef333ed08c838ef51b8988e6553d
nagaraju.junna <nagaraju.junna90@gmail.com>
2025-03-23 11:27:03 +0530

    JWtFilter for authentication

--- api-gateway-service/api-gateway-service/src/main/java/com/nr/gateway/config/JwtUtil.java
+++ api-gateway-service/api-gateway-service/src/main/java/com/nr/gateway/config/JwtUtil.java
@@ -2,12 +2,8 @@ package com.nr.gateway.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
-import java.security.Key;
 import java.util.Date;
 
 @Component
